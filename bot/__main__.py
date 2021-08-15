8ac4b865f7702cf0a3d7a72c74136812bdd1df9c
anant-goel <85389190+anant-goel@users.noreply.github.com>
2021-08-15 20:06:57 +0530

    Add files via upload

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,7 +64,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code> CPU: <code>{cpuUsage}</code> RAM: <code>{memory}</code> DISK: <code>{disk}</code>", context.bot, update)
+            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
         else :
             update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
     else :
